fd51d0d88addda906ce4a2c64233299f4058a652
Hrit-Codes <privhritamatya@gmail.com>
2026-06-16 14:56:36 +0545

    Implemented CRUD Destination routes

--- src/routes/destinationRoutes.ts
+++ src/routes/destinationRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { isAuthenticated } from "../middleware/auth.middleware";
-import { createDestination } from "../controller/destination.controller";
+import { isAdmin, isAuthenticated } from "../middleware/auth.middleware";
+import { createDestination, deleteDestination, getDestinationById, getDestinations, toggleDestinationActiveStatus, updateDestination } from "../controller/destination.controller";
 
 const router=Router();
 
-router.post("/create",isAuthenticated,createDestination);
+router.post("/create",isAuthenticated,isAdmin,createDestination);
+router.delete("/delete/:id",isAuthenticated,isAdmin,deleteDestination);
+router.patch("/toggleActiveStatus/:id",isAuthenticated,isAdmin,toggleDestinationActiveStatus);
+router.put("/updateDestination/:id",isAuthenticated,isAdmin,updateDestination);
+router.get("/getDestinationById/:id",isAuthenticated,isAdmin,getDestinationById);
+router.get("/getDestinations",isAuthenticated,isAdmin,getDestinations);
 
 export default router;
