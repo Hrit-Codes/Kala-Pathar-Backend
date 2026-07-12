4dc62a1a2702a578a8cbff3df935cfe473d2a85a
Hrit-Codes <privhritamatya@gmail.com>
2026-07-12 14:47:39 +0545

    Fixed unsubscribe logic and added check subscription status route

--- src/routes/subscriberRoutes.ts
+++ src/routes/subscriberRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getAllSubscribers, subscribe, unsubscribeByEmail } from "../controller/subscriber.controller";
+import { checkSubscriptionStatus, getAllSubscribers, subscribe, unsubscribe } from "../controller/subscriber.controller";
 import { isAdmin, isAuthenticated } from "../middleware/auth.middleware";
 
 const router=Router();
 
 router.post("/subscribe",subscribe);
-router.get("/unsubscribe/:token",unsubscribeByEmail);
+router.get("/check",checkSubscriptionStatus);
+router.patch("/unsubscribe",unsubscribe);
 router.get("/",isAuthenticated, isAdmin, getAllSubscribers);
 
 export default router;
